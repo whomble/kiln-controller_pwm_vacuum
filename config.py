f84111e320925adb3dc2719c13ab383b73c94352
Oneil <90308987+whomble@users.noreply.github.com>
2023-06-08 11:28:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,8 @@ currency_type   = "€"   # Currency Symbol to show when calculating cost to run
 #   can use whichever GPIO you prefer/have available.
 
 ### Outputs
-gpio_heat = 12  # Switches zero-cross solid-state-relay
+gpio_heat = 17  # Switches zero-cross solid-state-relay
+gpio_heat_pwm = 18 #pwm pour la gestion proportionnelle de la pûissance
 
 ### Thermocouple Adapter selection:
 #   max31855 - bitbang SPI interface
